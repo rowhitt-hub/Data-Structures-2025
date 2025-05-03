c35670d1f5c8567e6dfddce4f90c3f583ab0d70e
rowhitt-hub <rohitsrinivasan01@gmail.com>
2025-05-03 11:37:12 +0530

    Create L6Q1.cpp

---
+++ L6Q1.cpp
@@ -0,0 +1,10 @@
+/*
+Write a separate C++ menu-driven program to implement stack ADT using a character
+array of size 5. Maintain proper boundary condiƟons and follow good coding pracƟces. Stack
+ADT has the following operaƟons,
+
+1. Push
+2. Pop
+3. Peek
+4. Exit 
+*/
